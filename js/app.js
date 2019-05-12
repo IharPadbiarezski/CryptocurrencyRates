a466599966f7f739ac157ab27a41f23dc7fe3c53
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-12 10:27:30 +0300

    feat: reading values from the form

--- js/app.js
+++ js/app.js
@@ -2,3 +2,25 @@
 
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
+
+// Create the varibales
+
+const form = document.getElementById("form");
+
+// Add Event Listener
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // read currency
+  const currencySelect = document.getElementById("currency").value;
+  // read cryptocurrency
+  const cryptoCurrencySelect = document.getElementById("cryptocurrency").value;
+
+  // Validate that the selects have something
+  if (currencySelect === "" || cryptoCurrencySelect === "") {
+    // display an error
+    console.log("error");
+  } else {
+    console.log(currencySelect + ": " + cryptoCurrencySelect);
+  }
+});
